eaaace6babd82ec99b12ab8e7e6ff56e47c0dfa0
Kingnels <ifeanyiibe2022@gmail.com>
2022-09-09 12:56:05 +0100

    more commit

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,5 @@
-const images= ["./images/1.jpg","./images/2.jpg","./images/3.jpg"];
+const images= ["./images/1.jpg","./images/2.jpg","./images/3.jpg","./images/4.jpg","./images/5.jpg",
+"./images/6.jpg","./images/7.jpg","./images/8.jpg","./images/9.jpg","./images/10.jpg",];
 const img = document.querySelector("#image");
 const leftNavBtn = document.querySelector("#left-btn");
 const rightNavBtn = document.querySelector("#right-btn");
